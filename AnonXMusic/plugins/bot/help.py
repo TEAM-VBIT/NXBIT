98b7bfabd1f80009900e87988f76f8a5dccb459f
ᯓ꯭𓆰꯭×꯭🔥⁠⁠⁠𝐕꯭𝐀꯭𝐌꯭𝐏꯭𝐈꯭𝐑꯭𝐄꯭ 𝐊꯭𝐈꯭𝐍꯭𝐆 ꯭🌾꯭ <mdkhateeb07302@gmail.com>
2026-06-13 18:51:23 +0530

    Update help.py

--- AnonXMusic/plugins/bot/help.py
+++ AnonXMusic/plugins/bot/help.py
@@ -219,7 +219,7 @@ async def helper_cb(client, CallbackQuery:CallbackQuery, _):
         
         try:
             await CallbackQuery.edit_message_text(
-                "🎤 **TTS Model Settings**\n\nSelect a voice model \n\n [Check out the samples here](https://t.me/amigr8/27)",
+                "🎤 **TTS Model Settings**\n\nSelect a voice model \n\n [Check out the samples here](https://t.me/llDEAR_NICKll)",
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.DEFAULT
             )
